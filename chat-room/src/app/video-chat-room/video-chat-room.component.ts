20420700da9e2e3e8b8bdf4010e73962f7d9a844
Nikola Bijelic <nikola.bijelic@eng.it>
2018-10-09 17:55:57 +0200

    enabeling video

--- chat-room/src/app/video-chat-room/video-chat-room.component.ts
+++ chat-room/src/app/video-chat-room/video-chat-room.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { WebcamUtil } from '../util/webcam.util';
 @Component({
   selector: 'app-video-chat-room',
   templateUrl: './video-chat-room.component.html',
   styleUrls: ['./video-chat-room.component.css']
 })
 export class VideoChatRoomComponent implements OnInit {
-
+  public showWebcam = true;
+ 
   constructor() { }
 
   ngOnInit() {
+    WebcamUtil.getAvailableVideoInputs()
+      .then((mediaDevices: MediaDeviceInfo[]) => {
+       
+      });
   }
-
-}
+  start() {
+    this.showWebcam = true;
+  }
+   
+    stop() {
+      this.showWebcam = false;
+    }
+    public toggleWebcam(): void {
+      this.showWebcam = !this.showWebcam;
+    }
+  }
